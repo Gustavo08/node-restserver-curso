596c45b0ed77eb6e8035f95b2ee9157ce851775e
Gustavo08 <gustavopacheco0809@gmail.com>
2018-07-18 19:03:39 -0500

    RestReserver - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,24 @@
 // Puerto
 // ===============================
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ===============================
+// Entorno
+// ===============================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ===============================
+// Base de datos
+// ===============================
+
+let urlDB;
+
+if ( process.env.NODE_ENV == 'dev' ) {
+  urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+  urlDB = 'mongodb://cafe_user:a123456@ds141661.mlab.com:41661/cafe_mongo';
+}
+
+process.env.URLDB = urlDB;
